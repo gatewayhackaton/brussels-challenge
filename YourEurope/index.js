89b029f350423228ea747a1f6c9147d24e85d03c
Claudio <redavid5221+github@gmail.com>
2019-03-31 03:08:42 +0200

    Add location call

--- YourEurope/index.js
+++ YourEurope/index.js
@@ -1,3 +1,7 @@
+//jQuery(document).ready(function($) {
+//  alert("Your location is: " + geoplugin_countryName() + ", " + geoplugin_region() + ", " + geoplugin_city());
+//});
+
 $('#exampleModal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget) // Button that triggered the modal
     var recipient = button.data('whatever') // Extract info from data-* attributes
@@ -5,7 +9,13 @@ $('#exampleModal').on('show.bs.modal', function (event) {
     // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
     var modal = $(this)
     modal.find('.modal-title').text('Report an obstacle for ' + recipient)
+
+    $.getJSON("http://api.db-ip.com/v2/free/self").then(addrInfo =>
+		modal.find('.modal-country').text(addrInfo.countryName)
+	);
+
   })
+
 
   $(".typeahead").typeahead({
     source: [
@@ -13,4 +23,4 @@ $('#exampleModal').on('show.bs.modal', function (event) {
       {id: "id2", name: "Italy"},
       {id: "id3", name: "Belgium"}
     ]
-  });
+  });  
